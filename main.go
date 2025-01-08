35a6c5728880c43518a2291006e3ade97f12ffde
MICHAELKITH <michaelkithinji27@gmail.com>
2025-01-08 21:53:07 +0300

    Add all changes

--- main.go
+++ main.go
@@ -6,23 +6,35 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
  type Todos struct{
-	id int
-	completed bool
-	desc string
+	ID int `json:"id"`
+	Completed bool `json:"completed"`
+	Body string `json:"body"`
 
  }
 func main() {
 	app := fiber.New()
 
+	todos:=[]Todos{}
+
 	
 
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.Status(200).JSON(fiber.Map{"msg":"Connected"})
 	})
 
-	app.Patch("/:id", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todos{}
+		if err := c.BodyParser(todo); err !=nil{
+			return err
+		}
+		if todo.Body== ""{
+			return c.Status(400).JSON(fiber.Map{"error":"Todo is required"})
+		}
+
+		todo.ID= len(todos)+1
+		todos = append(todos, *todo)
+		return c.Status(400).JSON(todo)
 	})
 
 	log.Fatal(app.Listen(":4000"))
