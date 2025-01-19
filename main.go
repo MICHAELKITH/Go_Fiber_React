3b9f0104b667ba8c53b0f784774ed5cb29343c50
MICHAELKITH <michaelkithinji27@gmail.com>
2025-01-19 23:27:40 +0300

    Add main changes

--- main.go
+++ main.go
@@ -5,10 +5,12 @@ import (
 
 	"github.com/MICHAELKITH/todo_app/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
 	app := fiber.New()
+	app.Use(logger.New())
 
 	// Setup routes
 	routes.SetupRoutes(app)
