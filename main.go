220f1360f0d4ddd6d1aeb1fda9dd4cdec5f1b92f
MICHAELKITH <michaelkithinji27@gmail.com>
2025-01-22 18:59:30 +0300

    Solving conflicts

--- main.go
+++ main.go
@@ -3,22 +3,22 @@ package main
 import (
 	"log"
 
+	"github.com/MICHAELKITH/todo_app/routes"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
 
-<<<<<<< HEAD
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
-=======
 	// Setup routes
 	routes.SetupRoutes(app)
 	
 	//app listening
->>>>>>> routes
+
 
 	log.Fatal(app.Listen(":3000"))
 }
