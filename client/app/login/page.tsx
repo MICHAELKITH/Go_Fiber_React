0aa6833d7d31627c9c56f54030809fbb7781b7a6
MICHAELKITH <michaelkithinji27@gmail.com>
2025-04-03 23:51:17 +0300

    Login changes

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -19,7 +19,7 @@ export default function Login() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:4000/api/login", {
+      const response = await fetch("http://localhost:4000/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
