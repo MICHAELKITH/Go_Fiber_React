fdafc4436b3f90322018d94e57c9c1c6410d60fa
MICHAELKITH <michaelkithinji27@gmail.com>
2025-06-15 23:25:07 +0300

    Update foreign keys

--- backend/controllers/todo.go
+++ backend/controllers/todo.go
@@ -1,93 +1,77 @@
 package controllers
 
 import (
-    "fmt"
-    "log"
+	"fmt"
+	"log"
 
-    "github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2"
 )
 
-// Add a UserID field to Todos struct
 type Todos struct {
-    ID        int    `json:"id"`
-    Completed bool   `json:"completed"`
-    Body      string `json:"body"`
-    UserID    string `json:"user_id"` // Add this field
+	ID        int    `json:"id"`
+	Completed bool   `json:"completed"`
+	Body      string `json:"body"`
 }
 
-// Initial slice
+// In-memory todo storage
 var todos []Todos
 
-// GetTodo fetches all todos for the logged-in user
+// GetTodo fetches all todos
 func GetTodo(c *fiber.Ctx) error {
-    userID := c.Locals("user_id")
-    if userID == nil {
-        return c.Status(401).JSON(fiber.Map{"error": "Unauthorized"})
-    }
-
-    userTodos := []Todos{}
-    for _, todo := range todos {
-        if todo.UserID == userID {
-            userTodos = append(userTodos, todo)
-        }
-    }
-    return c.Status(200).JSON(userTodos)
+	return c.Status(fiber.StatusOK).JSON(todos)
 }
 
-// AddTodo adds a new todo for the logged-in user
+// AddTodo adds a new todo
 func AddTodo(c *fiber.Ctx) error {
-    userID := c.Locals("user_id")
-    if userID == nil {
-        return c.Status(401).JSON(fiber.Map{"error": "Unauthorized"})
-    }
+	todo := &Todos{}
+	if err := c.BodyParser(todo); err != nil {
+		log.Println("Error parsing body:", err)
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid request body"})
+	}
 
-    todo := &Todos{}
-    if err := c.BodyParser(todo); err != nil {
-        log.Println("Error parsing body:", err)
-        return c.Status(400).JSON(fiber.Map{"error": "Invalid request body"})
-    }
-    if todo.Body == "" {
-        return c.Status(400).JSON(fiber.Map{"error": "Todo body is required"})
-    }
+	if todo.Body == "" {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Todo body is required"})
+	}
 
-    todo.ID = len(todos) + 1
-    todo.UserID = userID.(string)
-    todos = append(todos, *todo)
-    return c.Status(201).JSON(todo)
+	todo.ID = len(todos) + 1
+	todos = append(todos, *todo)
+	return c.Status(fiber.StatusCreated).JSON(todo)
 }
 
-// UpdateTodo toggles the completed status of a todo (only if owned by user)
+// UpdateTodo updates the completed status or body of a todo
 func UpdateTodo(c *fiber.Ctx) error {
-    userID := c.Locals("user_id")
-    if userID == nil {
-        return c.Status(401).JSON(fiber.Map{"error": "Unauthorized"})
-    }
-    id := c.Params("id")
+	id := c.Params("id")
+	update := &Todos{}
 
-    for i, todo := range todos {
-        if fmt.Sprint(todo.ID) == id && todo.UserID == userID {
-            todos[i].Completed = !todos[i].Completed
-            return c.Status(200).JSON(todos[i])
-        }
-    }
+	if err := c.BodyParser(update); err != nil {
+		log.Println("Error parsing body:", err)
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid request body"})
+	}
 
-    return c.Status(404).JSON(fiber.Map{"error": "Todo not found"})
+	for i, todo := range todos {
+		if fmt.Sprint(todo.ID) == id {
+			// Optional updates
+			if update.Body != "" {
+				todos[i].Body = update.Body
+			}
+			todos[i].Completed = update.Completed
+			return c.Status(fiber.StatusOK).JSON(todos[i])
+		}
+	}
+
+	return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Todo not found"})
 }
 
-// DeleteTodo removes a todo (only if owned by user)
+// DeleteTodo removes a todo
 func DeleteTodo(c *fiber.Ctx) error {
-    userID := c.Locals("user_id")
-    if userID == nil {
-        return c.Status(401).JSON(fiber.Map{"error": "Unauthorized"})
-    }
-    id := c.Params("id")
+	id := c.Params("id")
 
-    for i, todo := range todos {
-        if fmt.Sprint(todo.ID) == id && todo.UserID == userID {
-            todos = append(todos[:i], todos[i+1:]...)
-            return c.Status(200).JSON(fiber.Map{"success": true})
-        }
-    }
+	for i, todo := range todos {
+		if fmt.Sprint(todo.ID) == id {
+			todos = append(todos[:i], todos[i+1:]...)
+			return c.Status(fiber.StatusOK).JSON(fiber.Map{"success": true})
+		}
+	}
 
-    return c.Status(404).JSON(fiber.Map{"error": "Todo not found"})
+	return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Todo not found"})
 }
